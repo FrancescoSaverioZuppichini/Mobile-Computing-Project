8ec097752f4c33d9630c77de465b9a4ed9a2123b
HeapHop30 <alessia.ruggeri.30@gmail.com>
2017-11-22 10:38:43 +0100

    Add auth view with linked layout

--- client/MyApplication/app/src/main/java/com/example/vaevictis/myapplication/auth/AuthView.java
+++ client/MyApplication/app/src/main/java/com/example/vaevictis/myapplication/auth/AuthView.java
@@ -1,14 +1,35 @@
 package com.example.vaevictis.myapplication.auth;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-/**
- * Created by vaevictis on 15.11.17.
- */
+import com.example.vaevictis.myapplication.R;
 
-public class AuthView extends Activity implements AuthContract.View {
+public class AuthView extends AppCompatActivity implements AuthContract.View {
+
     @Override
     public void showError(String error) {
 
     }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.auth);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
 }
