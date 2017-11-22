0aca8c9475ccded260dd29e35575bdc54ec94638
FrancescoSaverioZuppichini <zuppif@usi.ch>
2017-11-22 11:01:06 +0100

    added seeder to the server

---
+++ server/seed.js
@@ -0,0 +1,62 @@
+var mongoose = require('mongoose')
+
+var seeder = require('mongoose-seed');
+
+DB_URL = 'mongodb://localhost/mobile_computing'
+
+seeder.connect(DB_URL, function () {
+
+  // Load Mongoose models
+  seeder.loadModels([
+    './models/User.js'
+  ])
+  // Clear specifixsed collections
+  seeder.clearModels(['User'], function () {
+    // Callback to populate DB once collections have been cleared
+    seeder.populateModels(data, function () {
+      seeder.disconnect();
+    })
+  })
+})
+
+
+const data = [{
+  'model': 'User',
+  'documents': [{
+      email: 'alessia@95Xd.com',
+      password: 'francesco<3',
+      role: 'VOLUNTEER',
+      medicInfo: {
+        blood: 'O+'
+      },
+      position: {
+        lat: 46.011095,
+        long: 8.957484199999953
+      }
+    },
+    {
+      email: 'zuppi@dio.com',
+      password: 'ivan',
+      role: 'USER',
+      medicInfo: {
+        blood: 'A+'
+      },
+      position: {
+        lat: 46.0108692,
+        long: 8.96078866
+      }
+    },
+    {
+      email: 'thomas@imanol.com',
+      password: 'jurgen',
+      role: 'VOLUNTEER',
+      medicInfo: {
+        blood: 'AB+'
+      },
+      position: {
+        lat: 46.0108692,
+        long: 8.96078866
+      }
+    }
+  ]
+}]
