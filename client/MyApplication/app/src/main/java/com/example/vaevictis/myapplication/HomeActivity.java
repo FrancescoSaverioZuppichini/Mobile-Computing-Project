02c041dbc2dc23301145921ff568213e2daeb810
FrancescoSaverioZuppichini <zuppif@usi.ch>
2017-12-01 21:19:42 +0100

    geolocalisation workkkksss

--- client/MyApplication/app/src/main/java/com/example/vaevictis/myapplication/HomeActivity.java
+++ client/MyApplication/app/src/main/java/com/example/vaevictis/myapplication/HomeActivity.java
@@ -1,20 +1,34 @@
 package com.example.vaevictis.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 
 import com.example.vaevictis.myapplication.GoogleAPI.GoogleAPIService;
+import com.example.vaevictis.myapplication.location.LocationController;
 
 public class HomeActivity extends AppCompatActivity {
-
+    LocationController locationController;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         GoogleAPIService googleAPIService = new GoogleAPIService(this);
-
+        locationController = new LocationController(this);
         googleAPIService.getClient();
+
+        locationController.initialise();
+        Button button = (Button) findViewById(R.id.position);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                locationController.
+//            }
+//        });
+
+
 
     }
 }
